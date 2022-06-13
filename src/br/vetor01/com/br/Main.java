c9826999b5959440ede7deaa40ff5adae5776ed2
Rosane Maciel <rosanemarciel@gmail.com>
2022-06-13 13:28:14 -0300

    first commit

---
+++ src/br/vetor01/com/br/Main.java
@@ -0,0 +1,15 @@
+package br.vetor01.com.br;
+
+public class Main {
+
+    public static void main(String[] args){
+        // Vetores compostos forma simples é prática.
+        int n[]={3,2,8,7,5,4};
+
+        for(int c=0;c<=5;c++){
+            System.out.print("c");
+            System.out.print(n[c]+ " ");
+        }
+
+    }
+}
